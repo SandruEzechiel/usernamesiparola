06df5ad545f9e62d75b587b3ef7c18b029df653f
SabdruEzechiel <ezechiel.sandru@emanuel.ro>
2023-10-25 19:59:06 +0300

    o

---
+++ usernamesiparola/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace usernamesiparola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string username = "Ezechiel";
+            const string password = "UEO";
+            string inputusername = null;
+            while (inputusername != "Ezechiel")
+            {
+                Console.Write("Username: ");
+                inputusername = Console.ReadLine();
+                if (inputusername != username)
+                {
+                    Console.WriteLine("User nu a fost gasit");
+                }
+                else
+                {
+                    Console.Write("Parola: ");
+                    string inputpassword = Console.ReadLine();
+                    if (inputpassword != password)
+                    {
+                        Console.WriteLine("Parola este gresita");
+                        Console.ReadKey();
+                        Console.Clear();
+                        inputusername = null; inputpassword = null;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bine ai venit, " + username + " !");
+                    }
+                }
+            }
+
+        }
+    }
+}
